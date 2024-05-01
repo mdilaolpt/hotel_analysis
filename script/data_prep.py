9317d7b6f26bc9cc3f848251ebb4ceeec29c000d
Danielle Ilao <mdilaolpt@gmail.com>
2024-05-02 04:20:01 +0800

    analysis updates

--- script/data_prep.py
+++ script/data_prep.py
@@ -1,10 +1,6 @@
 # importing packages
-
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import sqlite3
-
 
 # data
 # create a dataset from multiple CSV files using a SQL query

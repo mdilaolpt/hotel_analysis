cfb734d597f9b46979765efb7c35b37421aabcf8
Danielle Ilao <mdilaolpt@gmail.com>
2024-05-02 21:35:39 +0800

    analysis updated

--- script/data_prep.py
+++ script/data_prep.py
@@ -35,19 +35,19 @@ query = """
 hotel_df = pd.read_sql_query(query, conn)
 
 # additional metrics
-hotel_df['OCCUPANCY'] = hotel_df['RM_NTS'] / hotel_df['RMS_AVAIL'] * 100                       #occupancy rate      (rooms sold / total rooms available)
-hotel_df['ADR'] = hotel_df['RM_REV_AMT_LOC'] / hotel_df['RM_NTS']                              #average daily rate (revenue / number of room sold)
-hotel_df['REV_PAR'] = hotel_df['RM_REV_AMT_LOC'] / hotel_df['RMS_AVAIL']                       #revenue per available room (revenue / total rooms available)
 hotel_df['CONVERTED_REV'] = hotel_df['RM_REV_AMT_LOC'] / hotel_df['EXCH_RATE_AMT']             #converted revenue to USD (revenue in original curr / exchange rate)
-
-# check hotel df
-print(hotel_df.head())
+hotel_df['OCCUPANCY'] = hotel_df['RM_NTS'] / hotel_df['RMS_AVAIL'] * 100                      #occupancy rate      (rooms sold / total rooms available)
+hotel_df['ADR'] = hotel_df['CONVERTED_REV'] / hotel_df['RM_NTS']                              #average daily rate (revenue / number of room sold) USED CONVERTED_RATE TO STANDARDIZE CURRENCY
+hotel_df['REV_PAR'] = hotel_df['CONVERTED_REV'] / hotel_df['RMS_AVAIL']                       #revenue per available room (revenue / total rooms available) USED CONVERTED_RATE TO STANDARDIZE CURRENCY
 
 # end connection
 conn.close()
 
 # write the merged hotel_df.csv to the data folder as final table
-hotel_df.to_csv('data/hotel_df.csv', index=False)
+hotel_df.to_csv('/Users/danielle/hotel_performance_analysis/data/hotel_df.csv', index=False)
+
+# check hotel df
+print(hotel_df.head())
 
 # note that in a real project/reporting, this py file should be run whenever data is refreshed/updated so that hotel_df.csv will contain latest datapoints
 # for the sake of this project, we'll use this static code :)

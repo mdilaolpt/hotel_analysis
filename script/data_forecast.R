493616b4c75523776a4a6b4a6158bedb24d02ef3
Danielle Ilao <mdilaolpt@gmail.com>
2024-05-04 01:19:10 +0800

    forecast script update

--- script/data_forecast.R
+++ script/data_forecast.R
@@ -18,7 +18,7 @@ ukpil_df <- read_csv("data/hotel_UKPIL_data.csv")
 
 
 
-#Create FUNCTION for forecasting-------------------------------------------
+# REVENUE FORECAST FUNC-------------------------------------------
 hotel_forecast <- function(df,
                            forecast_horizon){
   
@@ -41,7 +41,8 @@ df <- df %>%
 model <- prophet(df,
                  growth = "linear",
                  algorithm = "Newton")
-#Forecast for 3 month ---------------------
+
+# Forecast 
 future <- make_future_dataframe(model, periods = forecast_horizon)
 future$cap <- round(mean(df$y[round(n * 0.5):n], na.rm = TRUE) + 2 * stats::sd(df$y[round(n * 0.5):n], na.rm =TRUE)) 
 
@@ -79,9 +80,63 @@ hotel_plot <- ggplot(combined_data, aes(x = ds)) +
 return(hotel_plot)
 }
 
+
+# ROOMS FORECAST FUNC---------------------------------
+
+occupancy_forecast <- function(df,
+                           forecast_horizon){
+  
+  names(df)[names(df) == "STY_DT"] <- "ds"
+  names(df)[names(df) == "OCCUPANCY"] <- "y"
+  
+  # fit the Prophet model with logistic growth
+  model <- prophet(df,
+                   growth = "linear",
+                   algorithm = "Newton")
+  #Forecast 
+  future <- make_future_dataframe(model, periods = forecast_horizon)
+  future$cap <- 100
+  
+  forecast <- predict(model, future)
+  forecast <- forecast %>%
+    select(ds, yhat, yhat_upper, yhat_lower)
+  
+  # Convert ds column in df to Date format
+  df$ds <- as.Date(df$ds)
+  
+  # Extract actual values from the original dataframe
+  actual_values <- df[c("ds", "y")]
+  
+  # Extract predicted values from the forecast dataframe
+  predicted_values <- forecast[c("ds", "yhat")]
+  
+  # Convert ds column in forecast to Date format
+  predicted_values$ds <- as.Date(predicted_values$ds)
+  
+  # Combine actual and predicted values
+  combined_data <- left_join(forecast, df, by = "ds")
+  
+  hotel_name <-df %>%
+    select(HTL_CD) %>%
+    distinct() %>%
+    as.character()
+  
+  # Plot actual and predicted values
+  occupancy_plot <- ggplot(combined_data, aes(x = ds)) +
+    geom_line(aes(y = y, color = "Actual")) +
+    geom_line(aes(y = yhat, color = "Predicted")) +
+    labs(x = "Date", y = "Values", color = "Data Type") +
+    ggtitle(glue::glue("{hotel_name} Hotel Actual vs Predicted Occupancy Values for {forecast_horizon} days"))
+  
+  return(occupancy_plot)
+}
+
+
 #Forecasting for ICWAS hotel--------------------------
 setwd("~/hotel_performance_analysis/imgs") #to make sure that plots will be saved in /imgs
 
+
+## Revenue---------------------------------------------
 # 3 months
 ICWAS_forecast <-  hotel_forecast(icwas_df,
                                   forecast_horizon = 90)
@@ -92,8 +147,23 @@ ICWAS_forecast <-  hotel_forecast(icwas_df,
                                   forecast_horizon = 360)
 ggsave("icwas_forecast_1yr.png", plot = ICWAS_forecast)
 
+## Occupancy-------------------------------------------
+ICWAS_occ_forecast <-  occupancy_forecast(icwas_df,
+                                  forecast_horizon = 90)
+ggsave("icwas_occ_forecast_3mos.png", plot = ICWAS_occ_forecast)
+
+# 1 year
+ICWAS_occ_forecast <-  occupancy_forecast(icwas_df,
+                                  forecast_horizon = 360)
+ggsave("icwas_occ_forecast_1yr.png", plot = ICWAS_occ_forecast)
+
+
+
+
+
 #Forecasting for EXIUS hotel--------------------------
 
+## Revenue----------------------------------------------
 # 3 months
 EXIUS_forecast <-  hotel_forecast(exius_df,
                                   forecast_horizon = 90)
@@ -104,8 +174,22 @@ EXIUS_forecast <-  hotel_forecast(exius_df,
                                   forecast_horizon = 360)
 ggsave("exius_forecast_1yr.png", plot = EXIUS_forecast)
 
+## Occupancy-------------------------------------------
+EXIUS_occ_forecast <-  occupancy_forecast(exius_df,
+                                          forecast_horizon = 90)
+ggsave("exius_occ_forecast_3mos.png", plot = EXIUS_occ_forecast)
+
+# 1 year
+EXIUS_occ_forecast <-  occupancy_forecast(exius_df,
+                                          forecast_horizon = 360)
+ggsave("exius_occ_forecast_1yr.png", plot = EXIUS_occ_forecast)
+
+
+
+
 #Forecasting for GCBJN hotel--------------------------
 
+#Revenue ----------------------------
 # 3 months
 GCBJN_forecast <-  hotel_forecast(gcbjn_df,
                                   forecast_horizon = 90)
@@ -115,6 +199,19 @@ ggsave("gcbjn_forecast_3mos.png", plot = GCBJN_forecast)
 GCBJN_forecast <-  hotel_forecast(gcbjn_df,
                                   forecast_horizon = 360)
 ggsave("gcbjn_forecast_1yr.png", plot = GCBJN_forecast)
+
+#Occupancy --------------------------
+GCBJN_occ_forecast <-  occupancy_forecast(gcbjn_df,
+                                          forecast_horizon = 90)
+ggsave("gcbjn_occ_forecast_3mos.png", plot = GCBJN_occ_forecast)
+
+# 1 year
+GCBJN_occ_forecast <-  occupancy_forecast(gcbjn_df,
+                                          forecast_horizon = 360)
+ggsave("gcbjn_occ_forecast_1yr.png", plot = GCBJN_occ_forecast)
+
+
+
 
 #Forecasting for PHEGA hotel--------------------------
 
@@ -127,6 +224,19 @@ ggsave("phega_forecast_3mos.png", plot = PHEGA_forecast)
 PHEGA_forecast <-  hotel_forecast(phega_df,
                                   forecast_horizon = 360)
 ggsave("phega_forecast_1yr.png", plot = PHEGA_forecast)
+
+
+#Occupancy --------------------------
+phega_occ_forecast <-  occupancy_forecast(phega_df,
+                                          forecast_horizon = 90)
+ggsave("phega_occ_forecast_3mos.png", plot = phega_occ_forecast)
+
+# 1 year
+phega_occ_forecast <-  occupancy_forecast(phega_df,
+                                          forecast_horizon = 360)
+ggsave("phega_occ_forecast_1yr.png", plot = phega_occ_forecast)
+
+
 
 
 #Forecasting for UKPIL hotel--------------------------
@@ -143,3 +253,16 @@ UKPIL_forecast <-  hotel_forecast(ukpil_df,
 ggsave("ukpil_forecast_1yr.png", plot = UKPIL_forecast)
 
 
+#Occupancy --------------------------
+ukpil_occ_forecast <-  occupancy_forecast(ukpil_df,
+                                          forecast_horizon = 90)
+ggsave("ukpil_occ_forecast_3mos.png", plot = ukpil_occ_forecast)
+
+# 1 year
+ukpil_occ_forecast <-  occupancy_forecast(ukpil_df,
+                                          forecast_horizon = 360)
+ggsave("ukpil_occ_forecast_1yr.png", plot = ukpil_occ_forecast)
+
+
+#return wd to main folder
+setwd("~/hotel_performance_analysis/") 
